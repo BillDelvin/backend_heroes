24694f6837f5117d57e2a8921841423426d48d80
billdelvin <billdelvin6@gmail.com>
2020-04-29 19:45:17 +0700

    done

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-var logger = require("morgan");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const privateKey = "privateKey";
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+const bodyParser = require("body-parser")
+var logger = require("morgan")
+const mongoose = require("mongoose")
+require("dotenv").config()
+const jwt = require("jsonwebtoken")
+const privateKey = "privateKey"
 
-var indexRouter = require("./routes/index");
-const heroesRouter = require("./routes/heroesRouter");
-const userRouter = require("./routes/User");
+var indexRouter = require("./routes/index")
+const heroesRouter = require("./routes/heroesRouter")
+const userRouter = require("./routes/User")
 
-var app = express();
-const mongodConnect = process.env.DB_LOCAL;
-mongoose.connect(mongodConnect, { useNewUrlParser: true, useUnifiedTopology: true });
+var app = express()
+const mongodConnect = process.env.DB_LOCAL
+mongoose.connect(mongodConnect, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use("/", indexRouter);
-app.use("/user", userRouter);
-app.use("/heroes", validateUser, heroesRouter);
+app.use("/", indexRouter)
+app.use("/user", userRouter)
+app.use("/heroes", validateUser, heroesRouter)
 
 function validateUser(req, res, next) {
-	jwt.verify(req.headers["x-access-token"], privateKey, (err, decoded) => {
-
-		if (err) {
-			res.json(err);
-		} else {
-			req.body.userId = decoded.id;
-			next();
-		}
-	});
+  const token = req.headers["authorization"]
+  jwt.verify(token, privateKey, (err, decoded) => {
+    console.log(req.headers)
+    if (err) {
+      res.json(err)
+      console.log(err)
+    } else {
+      req.body.userId = decoded.id
+      next()
+    }
+  })
 }
 
-module.exports = app;
+module.exports = app
--- controllers/User.js
+++ controllers/User.js
@@ -1,66 +1,46 @@
-const user = require("../models/User");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const privateKey = "privatekey";
+const user = require("../models/User")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const privateKey = "privateKey"
 
 module.exports = {
-	create: (req, res, next) => {
-		user
-			.create({
-				email: req.body.email,
-				password: req.body.password,
-			})
-			.then((result) => res.json(result))
-			.catch((err) => {
-				throw err;
-			});
-	},
-	// getUserData: (req, res) => {
-	// 	user
-	// 		.find({})
-	// 		.then((response) => {
-	// 			res.json(response);
-	// 		})
-	// 		.catch((err) => {
-	// 			res.json(err);
-	// 		});
-	// },
-	// getUserDataById: (req, res) => {
-	// 	user
-	// 		.findById(req.params.userId)
-	// 		.then((response) => {
-	// 			res.json(response);
-	// 		})
-	// 		.catch((err) => {
-	// 			res.json(err);
-	// 		});
-	// },
-	authentication: (req, res, next) => {
-		user
-			.findOne({ email: req.body.email })
-			.then((response, err) => {
-				// console.log(response);
-				if (err) {
-					next(err);
-				} else {
-					if (response != null && bcrypt.compareSync(req.body.password, response.password)) {
-						jwt.sign(
-							{
-								id: response._id,
-							},
-							privateKey,
-							{ expiresIn: "10h" },
-							(err, token) => {
-								res.json(token);
-							},
-						);
-					} else {
-						res.json({ status: err });
-					}
-				}
-			})
-			.catch((err) => {
-				throw err;
-			});
-	},
-};
+  create: (req, res, next) => {
+    user
+      .create({
+        email: req.body.email,
+        password: req.body.password,
+      })
+      .then((result) => res.json(result))
+      .catch((err) => {
+        throw err
+      })
+  },
+  authentication: (req, res, next) => {
+    user
+      .findOne({ email: req.body.email })
+      .then((response, err) => {
+        console.log(response._id)
+        if (err) {
+          next(err)
+        } else {
+          if (response != null && bcrypt.compareSync(req.body.password, response.password)) {
+            jwt.sign(
+              {
+                id: response._id,
+              },
+              privateKey,
+              { expiresIn: 60 * 60 },
+              (err, token) => {
+                res.json(token)
+              }
+            )
+          } else {
+            res.json({ status: err })
+          }
+        }
+      })
+      .catch((err) => {
+        throw err
+      })
+  },
+}
